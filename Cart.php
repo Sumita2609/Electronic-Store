91f04b3a578f171b380741a59e64727494955030
Sumita2609 <72155593+Sumita2609@users.noreply.github.com>
2021-10-09 13:46:31 +0530

    Add files via upload

---
+++ Cart.php
@@ -0,0 +1,22 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>This is Cart</h1>
+  <button onclick="goBack()">black</button>
+</body>
+<script>
+  function goBack() {
+    window.history.back();
+  }
+</script>
+
+</html>
